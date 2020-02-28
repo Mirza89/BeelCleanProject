59487ea4a1954855e810625df0d0c35f099f6389
Mirza89 <41994509+Mirza89@users.noreply.github.com>
2020-02-28 04:22:59 +0100

    Updated Project
    Added many pages and linked them. Have much more to do. Tomorrow the project is ready to go for many changes.

---
+++ precache-manifest.d4912d863254dbfda09eac39aa88d3f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c73cb61ae6fbc5ffab37112c125d4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557e6b4d3257c64031da",
+    "url": "/static/css/2.8f657878.chunk.css"
+  },
+  {
+    "revision": "d366fc2fa9449ddb5aed",
+    "url": "/static/css/main.e7c3ef33.chunk.css"
+  },
+  {
+    "revision": "557e6b4d3257c64031da",
+    "url": "/static/js/2.61455ddd.chunk.js"
+  },
+  {
+    "revision": "4e18d12a3dbfe98d33be301d8b7d5495",
+    "url": "/static/js/2.61455ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d366fc2fa9449ddb5aed",
+    "url": "/static/js/main.f5c5cb25.chunk.js"
+  },
+  {
+    "revision": "dbf525a77132fcdd685f",
+    "url": "/static/js/runtime-main.797c44e1.js"
+  },
+  {
+    "revision": "8ded71cad6c71a0459c3866909bbeccd",
+    "url": "/static/media/Logo.8ded71ca.jpg"
+  },
+  {
+    "revision": "fa0f94c9b0e11fbf3d0a96051b938e6b",
+    "url": "/static/media/MainPicture.fa0f94c9.JPG"
+  }
+]);
