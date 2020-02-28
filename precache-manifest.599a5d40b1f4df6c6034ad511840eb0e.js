0fe37aff66768160512710495f44267e9394fc0a
Mirza89 <41994509+Mirza89@users.noreply.github.com>
2020-02-28 04:41:08 +0100

    Updates

---
+++ precache-manifest.599a5d40b1f4df6c6034ad511840eb0e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316751d883f56aab39e69b5be3e496a9",
+    "url": "/BeelCleanProject/index.html"
+  },
+  {
+    "revision": "557e6b4d3257c64031da",
+    "url": "/BeelCleanProject/static/css/2.8f657878.chunk.css"
+  },
+  {
+    "revision": "1750eb50d0206f3c331b",
+    "url": "/BeelCleanProject/static/css/main.e7c3ef33.chunk.css"
+  },
+  {
+    "revision": "557e6b4d3257c64031da",
+    "url": "/BeelCleanProject/static/js/2.61455ddd.chunk.js"
+  },
+  {
+    "revision": "4e18d12a3dbfe98d33be301d8b7d5495",
+    "url": "/BeelCleanProject/static/js/2.61455ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1750eb50d0206f3c331b",
+    "url": "/BeelCleanProject/static/js/main.d2552560.chunk.js"
+  },
+  {
+    "revision": "94491e1f26be5318a4ad",
+    "url": "/BeelCleanProject/static/js/runtime-main.6ad2ed54.js"
+  },
+  {
+    "revision": "8ded71cad6c71a0459c3866909bbeccd",
+    "url": "/BeelCleanProject/static/media/Logo.8ded71ca.jpg"
+  },
+  {
+    "revision": "fa0f94c9b0e11fbf3d0a96051b938e6b",
+    "url": "/BeelCleanProject/static/media/MainPicture.fa0f94c9.JPG"
+  }
+]);
